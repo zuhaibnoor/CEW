d88ad71cac71b9407139d3d32134efb383ef4fc9
Zuhaib Noor <130169065+zuhaibnoor@users.noreply.github.com>
2023-10-22 22:06:28 +0500

    Update Task2.c

--- Lab 1/Task2.c
+++ Lab 1/Task2.c
@@ -1,7 +1,31 @@
-/** Write a C program that does the following:
+/** TASK 2:
+Write a C program that does the following:
 A) Take two integers as input
 B)If both the numbers are greater than 5, add them.
 C)If either one of them is greater than 5, multiply them.
 D)Else print "Hello World". **/
 
+#include <stdio.h>
 
+int main (){
+
+    int num1, num2;
+    
+    printf("Enter integer 1: ");
+    scanf("%d", &num1);
+    
+    printf("Enter integer 2: ");
+    scanf("%d", &num2);
+    
+    if (num1 > 5 && num2 > 5){
+        printf("Their sum is: %d", num1+num2);
+    }
+    
+    else if(num1 > 5 || num2 > 5){
+        printf("Their product is: %d", num1*num2);
+    }
+    
+    else{
+        printf("Hello world.");
+    }
+}
