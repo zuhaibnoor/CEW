7e346c2bbb18691ac5f5e06b7a888faf516558ef
Zuhaib Noor <130169065+zuhaibnoor@users.noreply.github.com>
2023-10-22 22:01:02 +0500

    Update Task2.c

--- Lab 1/Task2.c
+++ Lab 1/Task2.c
@@ -1,1 +1,7 @@
+/** Write a C program that does the following:
+A) Take two integers as input
+B)If both the numbers are greater than 5, add them.
+C)If either one of them is greater than 5, multiply them.
+D)Else print "Hello World". **/
+
 
