807bcab425a2a39c3c74467ddbcced2d2a000a06
Zuhaib Noor <130169065+zuhaibnoor@users.noreply.github.com>
2023-10-23 00:45:18 +0500

    Create Exercise2.c

---
+++ Lab 1/Exercise2.c
@@ -0,0 +1,8 @@
+/** Write a C program that takes the height and width of a rectangle as an input from user
+and compute the perimeter and area of a rectangle. **/
+
+#include <stdio.h>
+
+int main(){
+
+}
