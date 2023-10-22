bc3ee253e1d1ea47c5a6e6eb627d6cdf6a3ec183
Zuhaib Noor <130169065+zuhaibnoor@users.noreply.github.com>
2023-10-23 00:54:16 +0500

    Update Exercise2.c

--- Lab 1/Exercise2.c
+++ Lab 1/Exercise2.c
@@ -3,6 +3,23 @@ and compute the perimeter and area of a rectangle. **/
 
 #include <stdio.h>
 
-int main(){
+int main() {
+
+    float height, width, area, perimeter;
+
+    printf("Enter height of the rectangle: ");
+    scanf("%f", &height);
+
+    printf("Enter width of the rectangle: ");
+    scanf("%f", &width);
+
+    area = height * width;
+    perimeter = 2 * (height + width);
+
+    printf("Area of the rectangle: %.2f\n", area);
+    printf("Perimeter of the rectangle: %.2f\n", perimeter);
+
+    return 0;
+}
 
 }
